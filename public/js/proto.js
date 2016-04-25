71897717b07d3020218b7448b4dea00522d54399
Simon Kinslow <skinslow@lhallet1.tmw.local>
2016-04-25 14:37:32 +0100

    New prototype functions added to contain flakes more

--- public/js/proto.js
+++ public/js/proto.js
@@ -14,16 +14,17 @@ var Snowing = function(v)
 {
 	this.flakeList = null;
 	this.max = v;
-}
+};
 
 Snowing.prototype.build = function()
 {
 	this.flakeList = new Array();
-}
+};
 
-var Flake = function()
+var Flake = function(f)
 {
 	this.instanceName = null;
+	this.completed = f;
 };
 
 Flake.prototype.build = function(n)
@@ -32,6 +33,7 @@ Flake.prototype.build = function(n)
 	this.html = getAsset("snow");
 	this.instanceName = 'flake_' + n;
 	this.arrayRef = n;
+	this.main = this;
 
 	// BASIC
 	this.dimensions = Math.round(Math.random() * (6 - 1) + 1);
@@ -46,13 +48,57 @@ Flake.prototype.build = function(n)
 
 	this.duration = Math.round(Math.random() * (8000 - 2000) + 2000);
 
+	this.hasListener = false;
 	this.refreshDelay;
-}
+};
 
 Flake.prototype.reference = function (o, i)
 {
 	this.outer = o;
 	this.inner = i;
+};
+
+Flake.prototype.birth = function()
+{
+	this.outer.style.width 			= this.dimensions + 'px';
+	this.outer.style.height 		= this.dimensions + 'px';
+	this.outer.style.opacity		= this.weight;
+	this.outer.style.transform	= 'translateX(' + this.positioning.x + 'px)';
+};
+
+Flake.prototype.place_start = function()
+{
+	this.inner.style.transform = 'translateY(' + this.positioning.ys + 'px)';
+	this.inner.style.transitionDuration = "0ms";
+};
+
+Flake.prototype.place_begin = function()
+{
+	this.inner.style.transform 					= 'translateY(' + this.positioning.yb + 'px)';
+	this.inner.style.transitionDuration = this.duration + 'ms';
+}
+
+Flake.prototype.place_end = function()
+{
+	this.inner.style.transform = 'translateY(' + this.positioning.ye + 'px)';
+
+	if(!this.hasListener)
+	{
+		this.hasListener = true;
+		this.inner.addEventListener("transitionend", this.completed, false);
+	}
+};
+
+Flake.prototype.fallComplete = function()
+{
+	this.place_start();
+	this.refreshDelay = setTimeout(this.fallRecyle, 20, this);
+};
+
+Flake.prototype.fallRecyle = function(flakeRef)
+{
+	flakeRef.inner.style.transitionDuration = flakeRef.duration + 'ms';
+	flakeRef.place_end();
 }
 
 function start_init(event)
@@ -76,7 +122,7 @@ function start_init(event)
 
 	for(var i = 0; i < fx.max; i++)
 	{
-		var f = new Flake();
+		var f = new Flake(animation_event);
 		f.build(i);
 
 		fx.flakeList.push(f);
@@ -98,8 +144,11 @@ function start_init(event)
 		var _in = _ou.querySelector(".snow");
 
 		fx.flakeList[j].reference(_ou, _in);
+		fx.flakeList[j].birth();
+		fx.flakeList[j].place_begin();
 
-		fx.flakeList[j].outer.style.width 			= fx.flakeList[j].dimensions + 'px';
+		/*
+		fx.flakeList[j].outer.style.width 		= fx.flakeList[j].dimensions + 'px';
 		fx.flakeList[j].outer.style.height 		= fx.flakeList[j].dimensions + 'px';
 		fx.flakeList[j].outer.style.opacity		= fx.flakeList[j].weight;
 		fx.flakeList[j].outer.style.transform	= 'translateX(' + fx.flakeList[j].positioning.x + 'px)';
@@ -107,11 +156,35 @@ function start_init(event)
 		fx.flakeList[j].inner.style.transform = 'translateY(' + fx.flakeList[j].positioning.yb + 'px)';
 
 		fx.flakeList[j].inner.style.transitionDuration = fx.flakeList[j].duration + 'ms';
+		*/
 	}
 
-	timer = setTimeout(animate_init, 1 * 1000);
+	timer = setTimeout(temp, 1 * 1000);
 }
 
+function temp()
+{
+	for(var i = 0; i < fx.flakeList.length; i++)
+	{
+		fx.flakeList[i].place_end();
+	}
+}
+
+function animation_event(event)
+{
+	var name = event.target.parentElement.classList[1];
+
+	for(var i = 0; i < fx.flakeList.length; i++)
+	{
+		if(fx.flakeList[i].instanceName === name)
+		{
+			fx.flakeList[i].fallComplete();
+			break;
+		}
+	}
+}
+
+/*
 function animate_init()
 {
 	eventList = new Array();
@@ -126,7 +199,9 @@ function animate_init()
 		eventList.push(sn);
 	}
 }
+*/
 
+/*
 function animate_event(event)
 {
 	var eventFlake = event.target.parentElement.classList[1];
@@ -152,7 +227,9 @@ function animate_event(event)
 		}
 	}
 }
+*/
 
+/*
 function animate_recyle(el, index)
 {
 	trace(el + " " + index);
@@ -160,6 +237,7 @@ function animate_recyle(el, index)
 	el.querySelector(".snow").style.transform = 'translateY(' + fx.flakeList[index].positioning.ye + 'px)';
 	el.querySelector(".snow").style.transitionDuration = fx.flakeList[index].duration + 'ms';
 }
+*/
 
 function getAsset(str)
 {
